aae2f512e05c08db752b25f805383bb7183da588
magzumAlmat <almat.magzum@gmail.com>
2023-05-03 15:24:19 +0600

    fc

---
+++ backend/data.js
@@ -0,0 +1,64 @@
+const data={
+    products:[
+        {
+            name:"Nike Slim shirt",
+            slug:'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            countInStock:10,
+            brand:'Nike',
+            raiting:4.5,
+            numReviews:10,
+            description:'high quality shirt',
+
+        },
+    
+        {
+            name:"Nike Slim shirt",
+            slug:'nike-slim-shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:120,
+            countInStock:10,
+            brand:'Nike',
+            raiting:4.5,
+            numReviews:10,
+            description:'high quality pants',
+
+        },
+
+
+        {
+            name:"Adidas Slim pants",
+            slug:'adidas-slim-pants',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:160,
+            countInStock:30,
+            brand:'Adidas',
+            raiting:4.7,
+            numReviews:10,
+            description:'high quality pants',
+
+        },
+
+        {
+            name:"Nike Slim pants",
+            slug:'nike-slim-pants',
+            category:'Shirts',
+            image:'/images/p4.jpg',
+            price:160,
+            countInStock:30,
+            brand:'Nike',
+            raiting:4.7,
+            numReviews:10,
+            description:'high quality pants',
+
+        },
+    
+    ]
+}
+
+
+export default data 
